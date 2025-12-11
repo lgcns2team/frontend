14a29ccf7017302c4aef67e8deae9f2143a92ff1
seongmin <seongmincho33@gmail.com>
2025-12-11 19:49:41 +0900

    간단 로그인 페이지

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from '../pages/landing/ui/LandingPage';
 import MapPage from '../pages/home/ui/Page';
+import LoginPage from '../pages/auth/ui/LoginPage';
+import SignupPage from '../pages/auth/ui/SignupPage';
 import './styles/App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/map" element={<MapPage />} />
       </Routes>
     </BrowserRouter>
