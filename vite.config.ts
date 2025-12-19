71c262da1271eeb8ec00a364572ea04a1a031b01
조재호 <he6465@naver.com>
2025-12-19 16:01:03 +0900

    env 설정
    env 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api/ai': {
-        target: 'http://localhost:8081',
-        changeOrigin: true,
-        ws: true,
-      },
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api/ai': {
+          target: env.VITE_AI_API_URL,
+          changeOrigin: true,
+          ws: true,
+        },
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
-  define: {
-    global: 'window',
-  },
+    define: {
+      global: 'window',
+    },
+  }
 })
